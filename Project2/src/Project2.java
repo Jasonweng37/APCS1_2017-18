b13336c46a9baffd933dd8ddf6f4efc89883eb90
jasonw37 <wuyi20122@gmail.com>
2017-08-22 09:28:40 -0700

    Second Project -- using Git from Eclipse

---
+++ Project2/src/Project2.java
@@ -0,0 +1,13 @@
+
+// Jason Weng
+// My second Java project. It will compare print and println
+// August 22, 2017
+
+public class Project2 {
+
+	public static void main(String[] args) {
+		System.out.println("hello -- this is project 2!!!");
+		System.out.print("LALALALALALALALALALALALALALALALALALA");
+	}
+
+}
